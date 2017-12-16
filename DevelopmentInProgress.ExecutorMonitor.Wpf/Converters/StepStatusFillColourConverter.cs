391dab27eee973a76263359683eb4a53960920d7
grantcolley <grantcolley@yahoo.com>
2017-12-16 22:25:49 +0000

    Work in progress.
    Work in progress.

---
+++ DevelopmentInProgress.ExecutorMonitor.Wpf/Converters/StepStatusFillColourConverter.cs
@@ -0,0 +1,38 @@
+﻿using DipRunner;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DevelopmentInProgress.ExecutorMonitor.Wpf.Converters
+{
+    public class StepStatusFillColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+                      object parameter, CultureInfo culture)
+        {
+            var status = (StepStatus)value;
+
+            switch (status)
+            {
+                case StepStatus.NotStarted:
+                    return new SolidColorBrush(Colors.LightGray);
+                case StepStatus.Initialise:
+                    return new SolidColorBrush(Colors.Yellow);
+                case StepStatus.InProgress:
+                    return new SolidColorBrush(Colors.LightGreen);
+                case StepStatus.Complete:
+                    return new SolidColorBrush(Colors.LightSkyBlue);
+                case StepStatus.Error:
+                    return new SolidColorBrush(Colors.Red);
+                default:
+                    throw new NotImplementedException($"Status {status}");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DevelopmentInProgress.ExecutorMonitor.Wpf/Converters/StepStatusStrokeColourConverter.cs
@@ -0,0 +1,38 @@
+﻿using DipRunner;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace DevelopmentInProgress.ExecutorMonitor.Wpf.Converters
+{
+    public class StepStatusStrokeColourConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+              object parameter, CultureInfo culture)
+        {
+            var status = (StepStatus)value;
+
+            switch (status)
+            {
+                case StepStatus.NotStarted:
+                    return new SolidColorBrush(Colors.DarkGray);
+                case StepStatus.Initialise:
+                    return new SolidColorBrush(Colors.Gold);
+                case StepStatus.InProgress:
+                    return new SolidColorBrush(Colors.MediumSeaGreen);
+                case StepStatus.Complete:
+                    return new SolidColorBrush(Colors.SkyBlue);
+                case StepStatus.Error:
+                    return new SolidColorBrush(Colors.DarkRed);
+                default:
+                    throw new NotImplementedException($"Status {status}");
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
